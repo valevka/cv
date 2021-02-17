ab95200ec883f6118c9ae5fd7b158fd177d3a09a
valevka <wshystko@mail.ru>
2021-02-17 21:38:40 +0300

    add indexJ

---
+++ index.js
@@ -0,0 +1,6 @@
+const loremText = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis labore illum doloremque quae nostrum illo aut suscipit. Sunt atque aspernatur soluta natus impedit sapiente ipsam quaerat dolorum debitis, tempore dolorem.';
+const sections = [
+    { type: 'title', value: 'Hello World!' },
+    { type: 'text', value: loremText },
+    { type: 'columns', value: [loremText, loremText, loremText] }
+];
