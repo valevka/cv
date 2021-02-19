480d24ed23942c67f5ff8079d481429a6778d019
valevka <wshystko@mail.ru>
2021-02-19 19:40:51 +0300

    html ans js navbar

--- index.js
+++ index.js
@@ -1,42 +1,56 @@
-const loremText = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis labore illum doloremque quae nostrum illo aut suscipit. Sunt atque aspernatur soluta natus impedit sapiente ipsam quaerat dolorum debitis, tempore dolorem.';
+const loremText = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia excepturi commodi impedit, possimus nulla vitae et ex. Quam nisi minima cum natus rem esse voluptatum laborum eveniet sequi. Quo, quibusdam.';
 const sections = [
-    { type: 'title', value: 'Hello World!' },
+    { type: 'title', value: 'Hello world!' },
     { type: 'text', value: loremText },
     { type: 'columns', value: [loremText, loremText, loremText] }
 ];
 
 const $content = document.querySelector('#cv');
 
-section.forEach(section => {
+sections.forEach(section => {
     let html = '';
-    if (section.type === 'title') {
-        let html = `
- <section class="row">
-                <div class="col-sm">
-                    <h2>${section.value}</h2>
-                </div>
-            </section>
- `
-    }
-    esle
-    if (section.type === 'text') {
 
-        esle
-        if (section.type === 'columns')
+    if (section.type === 'title') {
+        html = title(section);
+    } else if (section.type === 'text') {
+        html = text(section);
+    } else if (section.type === 'columns') {
+        html = columns(section);
     }
-    $content.insertAdjacentHTML('beforeens', html)
+
+    $content.insertAdjacentHTML('beforeend', html)
 })
 
 function title(section) {
     return `
     <section class="row">
-                   <div class="col-sm">
-                       <h2>${section.value}</h2>
-                   </div>
-               </section>
-    `
+        <div class="col-sm">
+            <h2>${section.value}</h2>
+        </div>
+    </section>
+`;
 }
 
 function text(section) {
-    return
+    return `
+    <section class="row">
+        <div class="col-sm">
+            <p>${section.value}</p>
+        </div>
+    </section>
+    `;
+}
+
+function columns(section) {
+    const html = section.value.map(
+        item => {
+            return `<div class="col-sm"><p>${item}</p></div>`
+        }
+    );
+
+    return `
+    <section class="row">
+        ${html.join('')}
+    </section>
+    `;
 }
